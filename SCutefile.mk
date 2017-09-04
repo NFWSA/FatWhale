51d74dde8cf83e36adf835de362d33736957a766
SurgeNight <surgenight@hotmail.com>
2017-09-04 20:48:28 +0800

    continue to implement. finish command line argument process.

--- SCutefile.mk
+++ SCutefile.mk
@@ -1,25 +1,28 @@
 # This is a demo for my UnknownFuture repo
 project a b c
 compile cxx g++
-compile cxxflags -std=c++11 -I.. -I../..
+compile cxxflags -std=c++11 -I../UnknownFuture
 compile c gcc
 #fdf
 a.target VariantTest
 a.path Test/
+b.path Test/
+c.path Test/Log/
 b.target ColorTableTest
 c.target LoggerTest
+b.cxxflags -std=c++11 -static -s
 a {
-	../UnknownFuture//Test/VariantTest.cpp
-	../UnknownFuture//SNVariant/Variant.cpp
+	../UnknownFuture/Test/VariantTest.cpp
+	../UnknownFuture/SNVariant/Variant.cpp
 }
 b{
-	../UnknownFuture//Test/ColorTableTest.cpp
-	../UnknownFuture//SNLogger/ColorOutput/ColorTable.cpp
+	../UnknownFuture/Test/ColorTableTest.cpp
+	../UnknownFuture/SNLogger/ColorOutput/ColorTable.cpp
 }
 
 c {
-	../UnknownFuture//Test/LoggerTest.cpp
-	../UnknownFuture//SNLogger/ColorOutput/ColorTable.cpp
-	../UnknownFuture//SNLogger/Logger.cpp
-	../UnknownFuture//SNLogger/FormatLogger.cpp
+	../UnknownFuture/Test/LoggerTest.cpp
+	../UnknownFuture/SNLogger/ColorOutput/ColorTable.cpp
+	../UnknownFuture/SNLogger/Logger.cpp
+	../UnknownFuture/SNLogger/FormatLogger.cpp
 }
